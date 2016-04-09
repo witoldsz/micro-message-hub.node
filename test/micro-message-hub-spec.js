429ec776d77fdda0158457998497804cbd31dc29
Witold Szczerba <witoldsz.dev@gmail.com>
2016-04-09 15:23:30 +0200

    travis-ci

--- test/micro-message-hub-spec.js
+++ test/micro-message-hub-spec.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var assert = require('assert');
+const assert = require('assert');
+const amqp = require('amqplib');
 
 describe('micro message hub', () => {
 
@@ -8,8 +9,25 @@ describe('micro message hub', () => {
 
   });
 
-  it('should return -1 when the value is not present', () => {
-
+  it('should verify the infrastructure', (done) => {
+    Object.keys(process.env).sort().forEach(k => console.log(k + ': ' + process.env[k]));
+    Promise.resolve()
+      .then(() => amqp.connect())
+      .then(conn => conn.createChannel())
+      .then(chan => {
+        chan.assertQueue('tasks');
+        return chan.sendToQueue('tasks', new Buffer('something to do'));
+      })
+      .then(() => amqp.connect())
+      .then(conn => conn.createChannel())
+      .then(chan => {
+        chan.assertQueue('tasks');
+        chan.consume('tasks', msg => {
+          assert.equal(msg.content.toString(), 'something to do');
+          chan.ack(msg);
+          done();
+        });
+      })
   });
 
 });
